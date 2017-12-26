8ac6980203a85a7dd8b0a5ccd105bb411eb113eb
mnkych <mnkych@gmail.com>
2017-12-26 23:11:09 +0700

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../css/modern-business.css','../vendor/bootstrap/css/bootstrap.min.css']
 })
 export class AppComponent {
   title = 'Thai3K';
