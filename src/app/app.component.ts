5f9115b4b3a8c5f6ce989eb8ef318c2a51e4dda3
Pome <weerapat.laor@gmail.com>
2017-12-27 13:03:28 +0700

    git commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../css/modern-business.css','../vendor/bootstrap/css/bootstrap.min.css']
+  styleUrls: ['../vendor/bootstrap/css/bootstrap.min.css', '../css/modern-business.css']
 })
 
 export class AppComponent {
